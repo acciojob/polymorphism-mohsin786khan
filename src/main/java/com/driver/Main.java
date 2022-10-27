d969a4dd72facb2732de704006eea51fff570100
mohsin khan <khan.mohsin.00001@gmail.com>
2022-10-27 14:22:48 +0530

    polymorphism code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,25 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String args[]) {
+
+        Product p = new Product();
+        int ans1 = p.product(1, 2);
+        int ans2 = p.product(1,2,3);
+        double ans3 = p.product(1.5, 1.2);
+        System.out.println(ans1 + " " + ans2 + " " + ans3);
+    }
+        public static class Product{
+            public int product(int x, int y){
+               return x*y;
+            }
+
+            public int product(int x, int y, int z){
+                return x*y*z;
+            }
+
+            public double product(double x , double y){
+                return x*y;
+            }
+        }
 }
